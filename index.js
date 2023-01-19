14f726f01df494101040df3b8eb5fc1673f69715
Gulshan7777 <sharmagulshan252@gmail.com>
2023-01-19 17:55:37 +0530

    port updated

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use("/user",userRouter)
 app.use('/products',productRoute)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT||3000;
 app.listen(PORT,()=>{
     connection
     .then(()=>{
